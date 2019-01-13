792894c557272f992a6d7c937484396c6e009781
Yasin Shuman <email@yasin.io>
2019-01-12 19:40:59 -0800

    added search and insert methods for nodes

--- main.go
+++ main.go
@@ -12,3 +12,39 @@ func main() {
 	tree := &Node{6, nil, nil}
 	fmt.Println(tree.Key)
 }
+
+func (n *Node) Search(key int) bool {
+	// if the node is nil, the key does not exist
+	if n == nil {
+		return false
+	}
+	//if the key is greater than the node we are at move to the right
+	if n.Key < key {
+		//move right
+		return n.Right.Search(key)
+	} else if n.Key > key {
+		//if the key is less than the node we are at move to the left
+		//move left
+		return n.Left.Search(key)
+	}
+	//you found the key if it is equal to your current node
+	return true
+}
+
+func (n *Node) Insert(key int) {
+	// if the key is greater than the node you are at and the right node is nil, insert. Otherwise,keep searching to the right.
+	if n.Key < key {
+		if n.Right == nil {
+			n.Right = &Node{Key: key}
+		} else {
+			n.Right.Insert(key)
+		}
+	} else if n.Key > key {
+		if n.Left == nil {
+			n.Left = &Node{Key: key}
+
+		} else {
+			n.Left.Insert(key)
+		}
+	}
+}
